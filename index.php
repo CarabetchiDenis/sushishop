8582a3a304c5d2d8b20885fca9774a465b80aede
Denis Carabetchi <carabetchidenis@gmail.com>
2021-09-09 13:42:02 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SushiShop</title>
+</head>
+<body>
+   <!-- sushi delivery from-->
+   <form action="" method="post"> 
+         
+     <div class="form">
+         <h3 class="titre">Welcome to SushiShop </h3>
+         <h4>Choisissez votre command</h4><br>
+
+         <!-- Choice option -->
+         
+         
+         <!--row 1-->
+         <tr>
+             <td><input type="radio" name="California" value="California">California Roll</td>
+             <td><input type="radio" name="Rainbow" value="Rainbow"> Rainbow Roll</td>
+             <td><input type="radio" name=" Est" value=" Est"> Est Roll</td>
+         </tr>  <br><br><br>
+         
+         <!--row 2-->
+         <tr>
+             <td><input type="radio" name="Philadelphia" value="Philadelphia">Philadelphia Roll</td>
+             <td><input type="radio" name="Valcano" value="Valcano"> Valcano Roll</td>
+             <td><input type="radio" name="Avocado" value="Avocado"> Avocado Roll</td>
+         </tr> <br> <br> <br>
+        
+         <!--Submit button-->
+
+            
+            <input type="submit" value="Submit order" name="submit">
+         
+         
+        
+        
+         
+         
+     </div>    
+</from>
+                
+
+        
+
+         
+
+
+
+
+
+         
+        
+
+
+</body>
+</html>
