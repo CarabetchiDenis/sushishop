e07fe8f25c61f79d070b655abf1220e09913be70
Denis Carabetchi <carabetchidenis@gmail.com>
2021-09-09 16:20:26 -0400

    modiied 'index.php + delivery.php

--- delivery.php
+++ delivery.php
@@ -1,54 +1,53 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delivery</title>
-</head>
-<body>
-    <h1> Thanks </h1>
+ session_start();
 
-    <?php
-    //verif
-      if (isset($_POST['submit'])) {
-        /*if (isset($_POST['C']) && isset($_POST['R']) && isset($_POST['E'])&& 
-        isset($_POST['P']) && isset($_POST['V'] && isset($_POST['A'])){*/
-
-        // creer variable
-      //  $C=htmlspecialchars($_POST['C']);
-      //  $R=htmlspecialchars($_POST['R']);
-      //  $E=htmlspecialchars($_POST['E']);
-      //  $P=htmlspecialchars($_POST['P']);
-      //  $V=htmlspecialchars($_POST['V']);
-      //  $A=htmlspecialchars($_POST['A']);
-      //  $txt = "SUSHISHOP";
+ //1. INPUT
+ //2. VALIDATE INPUT
+ //3. BUSINESS LOGIC (tvq tps)
+ //4. OUTPUT
+   echo "Thanks<br><br>";
+   echo "Votre commande est:<br><br><br>";
 
 
+ //verif
+    if (isset($_POST['California'])) {
+        echo $_POST['California'];
+    }
+    echo "<br><br>";
+    if(isset($_POST['Rainbow'])) {
+        echo $_POST['Rainbow'];
+    }
+    echo "<br><br>";
+    if(isset($_POST['Est'])) {
+        echo $_POST['Est'];
+    } 
+    echo "<br><br>";
+    if(isset($_POST['Philadelphia'])) {
+        echo $_POST['Philadelphia'];
+    } 
+    echo "<br><br>";
+    if(isset($_POST['Valcano'])) {
+        echo $_POST['Valcano'];
+    } 
+    echo "<br><br>";
+    if(isset($_POST['Avocado'])) {
+        echo $_POST['Avocado'];
+    } 
+    echo "<br><br>";   
 
-        //echo " your receipt today : <br> <br>   
-        //1) : " . $C . ",
-        //2) : " . $R . ",
-        //3) : " . $E . ",
-        //4) : " . $E . ",
-        //5) : " . $E . ",
-       // 6) : " . $E . ",
+
+
+
+     // creer variable
+        $txt = "SUSHISHOP";
+          
 
          
-        echo  "Total : <br>";
+        echo  "Sous-Total : <br>";
         echo  "TPS :  <br>";
         echo  "TVQ : <br>";
-        echo  " Total :  ";
+        echo  " Total : <br><br> ";
         echo  "Notre Slogan 'I love $txt'<br><br>";
-          
-       //}
-    }   
+        
+?>
     
-    
-    ?>
-    
-</body>
-</html>
--- index.php
+++ index.php
@@ -7,29 +7,27 @@
     <title>SushiShop</title>
 </head>
 <body>
-   <!-- sushi delivery from-->
-   <form action="delivery.php" method="post"> 
+      <!-- sushi delivery from-->
+    <form action="delivery.php" method="post"> 
          
         <div class="form">
          <h3 class="titre">Welcome to SushiShop </h3>
          <h4>Choisissez votre command</h4>
          <h5> Attention paquet de 6 morceaux </h5> <br>
 
-         <!-- Choice option -->
-         
-         
+         <!-- Choice option -->         
          <!--row 1-->
          <tr>
-             <td><input type="radio" name="C" value="1">California Roll   == "coût 5$"</td><br><br>
-             <td><input type="radio" name="R" value="2"> Rainbow Roll   ==  "coût 8$"</td><br><br>
-             <td><input type="radio" name=" E" value="3"> Est Roll  ==  "coût 18$"  </td><br><br>
+             <td><input type="radio" name="California" value="5" >California Roll   == "coût 5$"</td><br><br>
+             <td><input type="radio" name="Rainbow" value="8" > Rainbow Roll   ==  "coût 8$"</td><br><br>
+             <td><input type="radio" name="Est" value="18" > Est Roll  ==  "coût 18$"  </td><br><br>
          </tr>  <br><br><br>
          
          <!--row 2-->
          <tr>
-             <td><input type="radio" name="P" value="4">Philadelphia Roll    ==  "coût 28$"</td><br><br>
-             <td><input type="radio" name="V" value="5"> Valcano Roll    ==  "coût 15$"</td><br><br>
-             <td><input type="radio" name="A" value="6"> Avocado Roll    ==  "coût 10$"</td><br><br>
+             <td><input type="radio" name="Philadelphia" value="28" > Philadelphia Roll    ==  "coût 28$"</td><br><br>
+             <td><input type="radio" name="Valcano" value=" 15" > Valcano Roll    ==  "coût 15$"</td><br><br>
+             <td><input type="radio" name="Avocado" value="10" > Avocado Roll    ==  "coût 10$"</td><br><br>
          </tr> <br> <br> <br>
         
          <!--Submit button-->
