b6687509fb4a14aa90080cfaef79f52852fa0998
Denis Carabetchi <carabetchidenis@gmail.com>
2021-09-09 14:07:27 -0400

    modified index.php

--- index.php
+++ index.php
@@ -10,52 +10,34 @@
    <!-- sushi delivery from-->
    <form action="" method="post"> 
          
-     <div class="form">
+        <div class="form">
          <h3 class="titre">Welcome to SushiShop </h3>
-         <h4>Choisissez votre command</h4><br>
+         <h4>Choisissez votre command</h4>
+         <h5> Attention paquet de 6 morceaux </h5> <br>
 
          <!-- Choice option -->
          
          
          <!--row 1-->
          <tr>
-             <td><input type="radio" name="California" value="California">California Roll</td>
-             <td><input type="radio" name="Rainbow" value="Rainbow"> Rainbow Roll</td>
-             <td><input type="radio" name=" Est" value=" Est"> Est Roll</td>
+             <td><input type="radio" name="California" value="California">California Roll   == "coût 5$"</td><br><br>
+             <td><input type="radio" name="Rainbow" value="Rainbow"> Rainbow Roll   ==  "coût 8$"</td><br><br>
+             <td><input type="radio" name=" Est" value=" Est"> Est Roll  ==  "coût 18$"  </td><br><br>
          </tr>  <br><br><br>
          
          <!--row 2-->
          <tr>
-             <td><input type="radio" name="Philadelphia" value="Philadelphia">Philadelphia Roll</td>
-             <td><input type="radio" name="Valcano" value="Valcano"> Valcano Roll</td>
-             <td><input type="radio" name="Avocado" value="Avocado"> Avocado Roll</td>
+             <td><input type="radio" name="Philadelphia" value="Philadelphia">Philadelphia Roll    ==  "coût 28$"</td><br><br>
+             <td><input type="radio" name="Valcano" value="Valcano"> Valcano Roll    ==  "coût 15$"</td><br><br>
+             <td><input type="radio" name="Avocado" value="Avocado"> Avocado Roll    ==  "coût 10$"</td><br><br>
          </tr> <br> <br> <br>
         
          <!--Submit button-->
 
             
-            <input type="submit" value="Submit order" name="submit">
-         
-         
-        
-        
-         
-         
-     </div>    
-</from>
-                
-
-        
-
-         
-
-
-
-
-
-         
-        
-
+          <input type="submit" value="Submit order" name="submit">
+        </div>
+    </form>
 
 </body>
 </html>
