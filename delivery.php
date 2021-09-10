1c7d90efa1fb15e087965625fef1821c069bbdd1
Denis Carabetchi <carabetchidenis@gmail.com>
2021-09-10 14:48:28 -0400

    tout arranger en ordre dans file delivery.php: 1. INPUT  2. VALIDATE INPUT  3. BUSINESS LOGIC (tvq tps)  4. OUTPU

--- delivery.php
+++ delivery.php
@@ -5,82 +5,160 @@
  //2. VALIDATE INPUT
  //3. BUSINESS LOGIC (tvq tps)
  //4. OUTPUT
+
    echo "Thanks<br><br>";
    echo "Votre commande est:<br><br><br>";
 
 
- //verif
-    if (isset($_POST['California'])) {
+   ////////////////////////
+   ////Constants
+   ///////////////////////
+
+   $tps = 0.05;
+   $tvq = 0.10;
+
+   $price_california = 5;
+   $price_rainbow = 8;
+   $price_est = 3;
+   $price_philadelphia = 4;
+   $price_valcano = 9;
+   $price_avocado = 10;
+   $price_tp = 0.05;
+   $price_tq = 0.1;
+
+   $txt = "SUSHISHOP";
+
+
+   ////////////////////
+   ///end constants
+   ///////////////////
+
+
+   //getting the input
+
+   $QuantityCal =  0;
+   $QuantityRai =  0;
+   $QuantityEst =  0;
+   $QuantityPhil = 0;
+   $QuantityVal =  0;
+   $QuantityAvo =  0;
+
+   //input validation verif
+   $errors = Array();
+
+ 
+   if (isset($_POST['submit'])){
+       if (isset($_POST['California'])) {
+            $QuantityCal = htmlspecialchars($_POST['California']);
+            if (is_numeric($QuantityCal)){
+               if ($QuantityCal < 0){
+                   $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else {
+               $errors[] = "Quantity of California must be a number...";
+            }
+        }else {
+           $errors [] = "You did not submit the quantity of California...";
+        }
+        if (isset($_POST['Rainbow'])) {
+            $QuantityRai = htmlspecialchars($_POST['Rainbow']);
+            if (is_numeric($QuantityRai)){
+                if ($QuantityRai < 0){
+                    $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else {
+                $errors[] = "Quantity of Rainbow must be a number...";
+            }
+        }else {
+            $errors [] = "You did not submit the quantity of Rainbow...";
+        }
+        if (isset($_POST['Est'])) {
+            $QuantityEst = htmlspecialchars($_POST['Est']);
+            if (is_numeric($QuantityEst)) {
+                if ($QuantityEst < 0){
+                    $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else {
+                $errors[] = "Quantity of Est must be a number...";
+            }
+        }else {
+            $errors [] = "You did not submit the quantity of Est...";
+        } 
+        if (isset($_POST['Philadelphia'])) {
+            $QuantityPhil = htmlspecialchars($_POST['Philadelphia']);
+            if (is_numeric($QuantityPhil)) {
+                if ($QuantityPhil < 0){
+                    $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else{
+                $errors[] = "Quantity of Philadelphia must be a number...";
+            }
+        }else {
+            $errors [] = "You did not submit the quantity of Philadelphia...";
+        }
+        if (isset($_POST['Valcano'])) {
+            $QuantityVal = htmlspecialchars($_POST['Valcano']);
+            if (is_numeric($QuantityVal)) {
+                if ($QuantityVal < 0){
+                    $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else {
+                $errors[] = "Quantity of Valcano must be a number...";
+            }
+        }else {
+            $errors [] = "You did not submit the quantity of Valcano...";
+        }
+        if (isset($_POST['Avocado'])) {
+            $QuantityAvo = htmlspecialchars($_POST['Avocado']);
+            if (is_numeric($QuantityAvo)) {
+                if ($QuantityAvo < 0){
+                    $errors[] = "You can only buy a positive amount, otherwise go to another universe...";
+                }
+            }else {
+               $errors[] = "Quantity ofAvocado must be a number...";
+            }
+        } else {
+        $errors [] = "You did not submit the quantity of Avocado...";
+     } 
+    } else {
+        $errors[] = "Error, you did not submit the form...";
     }
-    if(isset($_POST['Rainbow'])) {
-    }    
-    if(isset($_POST['Est'])) {
-    } 
-    if(isset($_POST['Philadelphia'])) {
-    } 
-    if(isset($_POST['Valcano'])) {
-    } 
-    if(isset($_POST['Avocado'])) {
-    } 
-     
+    //output
+        
+    $number_or_errors = count($errors);
+    if ($number_or_errors > 0) {
+        //show the errors
+        for ($i = 0; $i < $number_or_errors; $i++) {
+            echo $errors[$i] . "<br>";
+        }
+    } else {
+        //show detailed receipt
+        echo "California   :"  . $QuantityCal .   "...ça coût:" .$price_california    *  $QuantityCal . "$<br>";
+        echo "Rainbow      :"  . $QuantityRai .   "...ça coût:" .$price_rainbow       *  $QuantityRai . "$<br>";
+        echo "Est          :"  . $QuantityEst .   "...ça coût:" .$price_est           *  $QuantityEst . "$<br>";
+        echo "Philadelphia :"  . $QuantityPhil .  "..ça coût:" .$price_philadelphia  *  $QuantityPhil . "$<br>";
+        echo "Valcano      :"  . $QuantityVal.    "...ça coût:" .$price_valcano       *  $QuantityVal .  "$<br>";
+        echo "Avocado      :"  . $QuantityAvo .   "...ça coût:" .$price_avocado       *  $QuantityAvo .  "$<br>";
+
+        //calculate TPS && TVQ
+        $sub_total = 0;
+        $sub_total += $price_california * $QuantityCal + $price_rainbow * $QuantityRai + $price_est * $QuantityEst + $price_philadelphia * $QuantityPhil + $price_valcano * $QuantityVal + $price_avocado * $QuantityAvo;
+
+        $total = $sub_total + $sub_total * $tps;
+        $pay_tps = $sub_total * $tps;
+        $pay_tvq = $total * $tvq;
+        $total += $total * $tvq;
+        echo "<br><br>Your subtotal is: " . $sub_total . "$<br><br>";
+        echo "TPS is: " . $pay_tps . "$<br>";
+        echo "TVQ is: " . $pay_tvq . "$<br><br>";
+        echo "________________________" . "<br><br>";
+        echo "Total is: " . $total . "$<br><br>";
     
-    // creer variable
-      $California = $_POST['California'];
-      $Rainbow = $_POST['Rainbow'];
-      $Est = $_POST['Est'];
-      $Philadelphia = $_POST['Philadelphia'];
-      $Valcano = $_POST['Valcano'];
-      $Avocado = $_POST['Avocado'];
-      $txt = "SUSHISHOP";
-      $QuantityCal = $_POST['California'];
-      $QuantityRai = $_POST['Rainbow'];
-      $QuantityEst = $_POST['Est'];
-      $QuantityPhil = $_POST['Philadelphia'];
-      $QuantityVal = $_POST['Valcano'];
-      $QuantityAvo = $_POST['Avocado'];
-      $C = 5;
-      $R = 8;
-      $E = 3;
-      $P = 4;
-      $V = 9;
-      $A = 10;
-      $TP = 0.05;
-      $TQ = 0.1;
+       echo "<a href='index.php'>BACK HOME</a><br><br><br>"; 
+       echo "Today is " . date("Y-m-d"). "<br>";
+       echo "Today is " . date("l");
 
-    // Calcul quantity  
-      $TotalCalifornia = $California *  $C;
-      $TotalRainbow = $Rainbow * $R;
-      $TotalEst = $Est * $E;
-      $TotalPhiladelphia = $Philadelphia * $P;
-      $TotalValcano = $Valcano * $V;
-      $TotalAvocado = $Avocado * $A;
-
-    //Calcul
-
-      $SousTotal = $TotalCalifornia + $TotalRainbow + $TotalEst + $TotalPhiladelphia + $TotalValcano + $TotalAvocado;
-      $TPS = $SousTotal * $TP;
-      $TPQ = $SousTotal * $TQ;
-      $Total = $SousTotal + $TPS + $TPQ;
-
-
-        echo " Sushi California Roll =  $QuantityCal  morceaux,  ça coût: $TotalCalifornia $ <br>
-               Sushi Rainbow Roll   = $QuantityRai  morceaux, ça coût:  $TotalRainbow $ <br>
-               Sushi Est Roll = $QuantityEst  morceaux, ça coût:  $TotalEst $ <br>
-               Sushi Philadelphia Roll = $QuantityPhil  morceaux, ça coût:  $TotalPhiladelphia $ <br>
-               Sushi Valcano Roll =  $QuantityVal  morceaux, ça coût:  $TotalValcano $ <br>
-               Sushi Avocado Roll  = $QuantityAvo  morceaux, ça coût:  $TotalAvocado $ <br><br>";
-
-       
-         
-        echo  "Sous-Total : $SousTotal  $ <br>";
-        echo  "TPS : $TPS  $  <br> ";
-        echo  "TVQ : $TPQ  $ <br>";
-        echo  " Total : $Total  $ <br><br> ";
-        echo  "Notre Slogan 'I love $txt'<br><br>";
-
-      
-
-       echo "<a href='index.php'>BACK HOME</a>"; 
+    }
+ 
         
 ?>
-    
--- index.php
+++ index.php
@@ -10,41 +10,40 @@
       <!-- sushi delivery from-->
         <form action="delivery.php" method="post"> 
          
-            <div class="form">
-             <h3 class="titre">Welcome to SushiShop </h3>
-             <h4>Choisissez votre command</h4>
-             <h5> Attention paquet de 0 morceaux par default </h5> <br>
+             <h1 class="titre">Welcome to SushiShop </h1>
+             <h2>Choisissez votre command</h2>
+             <h3> Attention paquet de 0 morceaux par default </h3> <br>
 
-              <!-- Choice option -->         
-                          
+              <!-- Choice option -->  
+                                          
                <label for="California">***California Roll***</label>
                <name="California" id="California"> 
-               <input type="number" name="California" id="California" min="0" value="0"><<< Select here == "coût  5$/morceaux"<br><br><br>
+               <input type="number" name="California"  min="0" value="0"><< Select here = "coût  5$/morc"<br><br><br>
                
                 <label for="Rainbow">***Rainbow Roll***</label>
                 <name="Rainbow" id="Rainbow">
-                <input type="number" name="Rainbow" id="Rainbow" min="0" value="0"><<< Select here == "coût  8$/morceaux"<br><br><br>
+                <input type="number" name="Rainbow"  min="0" value="0"><< Select here = "coût  8$/morc"<br><br><br>
              
                 <label for="Est">***Est Roll***</label>
                 <name="Est" id="Est">
-                <input type="number" name="Est" id="Est" min="0" value="0"><<< Select here == "coût  3$/morceaux"<br><br><br>
+                <input type="number" name="Est"  min="0" value="0"><< Select here = "coût  3$/morc"<br><br><br>
              
                 <label for="Philadelphia">***Philadelphia Roll***</label>
                 <name="Philadelphia" id="Philadelphia">
-                <input type="number" name="Philadelphia" id="Philadelphia" min="0" value="0"><<< Select here == "coût  4$/morceaux"<br><br><br>
+                <input type="number" name="Philadelphia" " min="0" value="0"><< Select here = "coût  4$/morc"<br><br><br>
              
                 <label for="Valcano">***Valcano Roll***</label>
                 <name="Valcano" id="Valcano">
-                <input type="number" name="Valcano" id="Valcano" min="0" value="0"><<< Select here == "coût  9$/morceaux"<br><br><br>
+                <input type="number" name="Valcano"  min="0" value="0"><< Select here = "coût  9$/morc"<br><br><br>
              
                 <label for="Avocado">***Avocado Roll***</label>
                 <name="Avocado" id="Avocado">
-                <input type="number" name="Avocado" id="Avocado" min="0" value="0"><<< Select here == "coût  10$/morceaux"<br><br><br>
-                <br> <br> <br>
+                <input type="number" name="Avocado"  min="0" value="0"><< Select here = "coût  10$/morc"<br><br><br>
+                <br> <br> 
         
                 <!--Submit button-->
                 <input type="submit" value="Submit order" name="submit">
-            </div>
+            
         </form>
 
     </body>
